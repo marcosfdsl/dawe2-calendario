0e22ebe0b9575ebff0da3482fec2f0fe53e9b35d
marcosfdsl <marcos.fernandez@live.u-tad.com>
2024-01-05 22:03:38 +0100

    ca1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { UsuarioComponent } from "./usuario/usuario.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, HeaderComponent, UsuarioComponent]
 })
 export class AppComponent {
   title = 'myApp';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { UsuarioComponent } from './usuario/usuario.component';
+import { CalendarioComponent } from './calendario/calendario.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path: '', title: 'Calendario', component: CalendarioComponent,
+    },
+    {
+        path: 'usuario', title: 'Usuario', component: UsuarioComponent,
+    },
+
+];
