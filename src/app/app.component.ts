dcc6b77be7b2bdcb937291d8bcb485b882000688
marcosfdsl <marcos.fernandez@live.u-tad.com>
2024-01-05 22:06:30 +0100

    ca2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
-import { UsuarioComponent } from "./usuario/usuario.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, HeaderComponent, UsuarioComponent]
+    imports: [CommonModule, RouterOutlet, HeaderComponent]
 })
 export class AppComponent {
   title = 'myApp';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,20 @@
 import { Routes } from '@angular/router';
-import { UsuarioComponent } from './usuario/usuario.component';
 import { CalendarioComponent } from './calendario/calendario.component';
+import { ProfesoresComponent } from './profesores/profesores.component';
+import { UsuarioComponent } from './usuario/usuario.component';
+import { MensajesComponent } from './mensajes/mensajes.component';
 
 export const routes: Routes = [
     {
     path: '', title: 'Calendario', component: CalendarioComponent,
     },
     {
+        path: 'profesores', title: 'Profesores', component: ProfesoresComponent,
+    },
+    {
         path: 'usuario', title: 'Usuario', component: UsuarioComponent,
     },
-
+    {
+        path: 'mensajes', title: 'Mensajes', component: MensajesComponent,
+    },
 ];
