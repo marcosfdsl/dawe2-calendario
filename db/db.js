e7b6c5182156ac847c01bc689cebf237849be7ed
marcosfdsl <marcos.fernandez@live.u-tad.com>
2024-02-06 22:50:18 +0100

    cb2

--- db/db.js
+++ db/db.js
@@ -3,6 +3,16 @@ const datos = [
         id: 1,
         nombre: "Silvia Orenes",
         correo: "silvia.orenes@live.u-tad.com"
+    },
+    {
+        id: 2,
+        nombre: "Nayra Benítez",
+        correo: "nayra.benitez@live.u-tad.com"
+    },
+    {
+        id: 3,
+        nombre: "Román Ontiyuelo",
+        correo: "roman.ontiyuelo@live.u-tad.com"
     }
 ];
 
