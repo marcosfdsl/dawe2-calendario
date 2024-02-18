485b0bcac649d60235955699372524306c8cf6a8
marcosfdsl <marcos.fernandez@live.u-tad.com>
2024-02-18 02:05:06 +0100

    cb5

--- db/db.js
+++ db/db.js
@@ -2,17 +2,62 @@ const profesores = [
     {
         id: 1,
         nombre: "Silvia Orenes",
-        correo: "silvia.orenes@live.u-tad.com"
+        correo: "silvia.orenes@live.u-tad.com",
+        usuario: "silvia.orenes",
+        telefono: "600000000",
+        cargo: "profesor/a",
+        asignaturas: [
+            {
+                idAsignatura: 1,
+                nombreAsignatura: "Programación",
+                cursoAsignatura: "DAW1",
+                diasAsignatura: ["Lunes", "Martes", "Jueves"]
+            },
+            {
+                idAsignatura: 2,
+                nombreAsignatura: "Programación",
+                cursoAsignatura: "DAM1",
+                diasAsignatura: ["Martes", "Miércoles", "Jueves"]
+            }
+        ]
     },
     {
         id: 2,
         nombre: "Nayra Benítez",
-        correo: "nayra.benitez@live.u-tad.com"
+        correo: "nayra.benitez@live.u-tad.com",
+        usuario: "nayra.benitez",
+        telefono: "600000001",
+        cargo: "profesor/a",
+        asignaturas: [
+            {
+                idAsignatura: 1,
+                nombreAsignatura: "Proyecto",
+                cursoAsignatura: "DAW2",
+                diasAsignatura: ["Lunes"]
+            }
+        ]
     },
     {
         id: 3,
         nombre: "Román Ontiyuelo",
-        correo: "roman.ontiyuelo@live.u-tad.com"
+        correo: "roman.ontiyuelo@live.u-tad.com",
+        usuario: "roman.ontiyuelo",
+        telefono: "600000002",
+        cargo: "profesor/a",
+        asignaturas: [
+            {
+                idAsignatura: 1,
+                nombreAsignatura: "Sistemas Informáticos",
+                cursoAsignatura: "DAW1",
+                diasClase: ["Lunes", "Miércoles"]
+            },
+            {
+                idAsignatura: 2,
+                nombreAsignatura: "Diseño de Interfaces",
+                cursoAsignatura: "DAW2",
+                diasAsignatura: ["Lunes", "Miércoles", "viernes"]
+            }
+        ]
     }
 ];
 
